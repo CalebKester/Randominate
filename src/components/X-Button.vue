6d5a3978f1a321f2d3e71d61d1d1508f21aac474
Caleb Kester <ckester@coachingactuaries.com>
2017-11-22 00:16:24 -0600

    removing uppercase from button

--- src/components/X-Button.vue
+++ src/components/X-Button.vue
@@ -2,7 +2,7 @@
   button(
     type="button"
     @click="click"
-    class="bg-orange text-grey hover:bg-orange-dark py-3 px-6 uppercase transition"
+    class="bg-orange text-grey hover:bg-orange-dark py-3 px-6 transition"
   )
     slot
 </template>
