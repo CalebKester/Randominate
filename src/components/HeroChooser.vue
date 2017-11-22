18aacbaea9b8a78b2ff6777474dd7fb914d0cf5e
Caleb Kester <ckester@coachingactuaries.com>
2017-11-21 21:34:29 -0600

    initial mock

--- src/components/HeroChooser.vue
+++ src/components/HeroChooser.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
-    <div v-for="hero in heroes" :key="hero.id">
-      <img :src="`/static/img/assets/${hero.name}.png`" :alt="hero.localized_name"  />
+    <div :class="$style.container">
+      <div :class="$style.wrap" :style="carousel">
+        <div 
+          v-for="(hero, index) in list" 
+          :key="hero.id"
+          :class="$style.hero"
+          :style="getTransform(index)"
+          >
+          <img
+            :src="`/static/img/assets/${hero.name}.png`" 
+          />
+            <!-- :alt="hero.localized_name"  
+            width="256"
+            height="144" -->
+        </div>
+      </div>
+    </div>
+    <div class="text-center">
+      <button class="bg-green p-4" @click="addHero(-1)">-</button>
+      <button class="bg-red p-4" @click="rotate(5)">Spin to win</button>
+      <button class="bg-green p-4" @click="addHero(1)">+</button>
     </div>
   </div>
 </template>
@@ -12,29 +31,88 @@ import { mapState } from "vuex";
 export default {
   data() {
     return {
-      weight: 225,
-      percent: 50
+      width: 208,
+      count: 7,
+      current: 1
     };
   },
   computed: {
     ...mapState(["heroes"]),
-    result() {
-      return Math.round(this.weight * this.percent / 100);
+    list() {
+      if (this.heroes) {
+        return this.heroes.slice(0, this.count);
+      }
+      return [];
+    },
+    θ() {
+      return 360 / this.count;
+    },
+    radius() {
+      return Math.round(this.width / 2 / Math.tan(Math.PI / this.count));
+    },
+    carousel() {
+      const deg = this.current * (this.θ * -1);
+      return `transform: translateZ(-${this.radius}px) rotateY(${deg}deg);`;
     }
   },
   methods: {
-    handleAdjustWeight(amount = 5) {
-      this.weight = this.weight + amount;
+    getTransform(index) {
+      const angle = this.θ * index;
+      // return `transform: rotateY(${angle}deg) translateZ(${this.radius}px)`;
+      return `transform: rotateY(${angle}deg) translateZ(${this.radius}px)`;
+    },
+    rotate() {
+      const random = Math.round(Math.random() * (this.count - 1) + this.count);
+      this.current = this.current + random;
+    },
+    addHero(count = 1) {
+      this.count = this.count + count;
     }
   }
 };
 </script>
+// https://www.youtube.com/watch?v=8WLiOXfXF1w
 
 <style module>
-.text {
-  font-size: 20vw;
+.container {
+  width: 210px;
+  height: 140px;
+  position: relative;
+  margin: 0 auto 40px;
+  border: 1px solid #ccc;
+  perspective: 1100px;
 }
-.text-small {
-  font-size: 5vw;
+.wrap {
+  /* position: relative;
+  height: 144px;
+  width: 800px;
+  margin: 0 auto;
+  background: hsla(0, 0%, 0%, 0.2);
+  transform-style: preserve-3d; */
+
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+}
+.hero {
+  display: block;
+  position: absolute;
+  width: 186px;
+  height: 116px;
+  left: 10px;
+  top: 10px;
+  border: 2px solid black;
+  line-height: 116px;
+  font-size: 80px;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+
+  img {
+    width: 186px;
+    height: 116px;
+  }
 }
 </style>
